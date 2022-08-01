7c398d8263edb50c4457bba5e1b3451e24c9934d
Francis <francischan@kingsphase.page>
2022-08-01 11:42:28 +0800

    'uploaded button test code'

--- tft_eSPI_touchscreen/src/button_test.cpp
+++ tft_eSPI_touchscreen/src/button_test.cpp
@@ -66,20 +66,23 @@ TFT_eSPI_Button key[15];
 void drawKeypad()
 {
   // Draw the keys
-  for (uint8_t row = 0; row < 5; row++) {
-    for (uint8_t col = 0; col < 3; col++) {
-      uint8_t b = col + row * 3;
+  tft.setFreeFont(LABEL1_FONT);
+  key[1].initButton(&tft,100,150,100,80,TFT_WHITE, TFT_RED, TFT_WHITE, keyLabel[0], KEY_TEXTSIZE);
+  key[1].drawButton();
+//   for (uint8_t row = 0; row < 5; row++) {
+//     for (uint8_t col = 0; col < 3; col++) {
+//       uint8_t b = col + row * 3;
 
-      if (b < 3) tft.setFreeFont(LABEL1_FONT);
-      else tft.setFreeFont(LABEL2_FONT);
+//       if (b < 3) tft.setFreeFont(LABEL1_FONT);
+//       else tft.setFreeFont(LABEL2_FONT);
 
-      key[b].initButton(&tft, KEY_X + col * (KEY_W + KEY_SPACING_X),
-                        KEY_Y + row * (KEY_H + KEY_SPACING_Y), // x, y, w, h, outline, fill, text
-                        KEY_W, KEY_H, TFT_WHITE, keyColor[b], TFT_WHITE,
-                        keyLabel[b], KEY_TEXTSIZE);
-      key[b].drawButton();
-    }
-  }
+//       key[b].initButton(&tft, KEY_X + col * (KEY_W + KEY_SPACING_X),
+//                         KEY_Y + row * (KEY_H + KEY_SPACING_Y), // x, y, w, h, outline, fill, text
+//                         KEY_W, KEY_H, TFT_WHITE, keyColor[b], TFT_WHITE,
+//                         keyLabel[b], KEY_TEXTSIZE);
+//       key[b].drawButton();
+//     }
+//   }
 }
 
 //------------------------------------------------------------------------------------------
@@ -196,78 +199,45 @@ void setup() {
 }
 
 //------------------------------------------------------------------------------------------
-
+bool start = false;
 void loop(void) {
   uint16_t t_x = 0, t_y = 0; // To store the touch coordinates
 
   // Pressed will be set true is there is a valid touch on the screen
   bool pressed = tft.getTouch(&t_x, &t_y);
-
+    
+    if(start == false){
   // / Check if any key coordinate boxes contain the touch coordinates
-  for (uint8_t b = 0; b < 15; b++) {
-    if (pressed && key[b].contains(t_x, t_y)) {
-      key[b].press(true);  // tell the button it is pressed
-    } else {
-      key[b].press(false);  // tell the button it is NOT pressed
-    }
-  }
-
-  // Check if any key has changed state
-  for (uint8_t b = 0; b < 15; b++) {
-
-    if (b < 3) tft.setFreeFont(LABEL1_FONT);
-    else tft.setFreeFont(LABEL2_FONT);
-
-    if (key[b].justReleased()) key[b].drawButton();     // draw normal
-
-    if (key[b].justPressed()) {
-      key[b].drawButton(true);  // draw invert
-
-      // if a numberpad button, append the relevant # to the numberBuffer
-      if (b >= 3) {
-        if (numberIndex < NUM_LEN) {
-          numberBuffer[numberIndex] = keyLabel[b][0];
-          numberIndex++;
-          numberBuffer[numberIndex] = 0; // zero terminate
+        for (uint8_t b = 0; b < 15; b++) {
+          if (pressed && key[b].contains(t_x, t_y)) {
+            key[b].press(true);  // tell the button it is pressed
+            Serial.println("Button Pressed");
+          } else {
+            key[b].press(false);  // tell the button it is NOT pressed
+          }
         }
-        status(""); // Clear the old status
-      }
 
-      // Del button, so delete last char
-      if (b == 1) {
-        numberBuffer[numberIndex] = 0;
-        if (numberIndex > 0) {
-          numberIndex--;
-          numberBuffer[numberIndex] = 0;//' ';
+        // Check if any key has changed state
+        for (uint8_t b = 0; b < 15; b++) {
+        
+          if (b < 3) tft.setFreeFont(LABEL1_FONT);
+          else tft.setFreeFont(LABEL2_FONT);
+
+          // if (key[b].justReleased()) key[b].drawButton();     // draw normal
+
+          if (key[b].justPressed()) {
+          //   key[b].drawButton(true);  // draw invert
+            start = true;
+            tft.fillScreen(TFT_BLACK);
+          }
+            delay(10); // UI debouncing
+          // }
         }
-        status(""); // Clear the old status
-      }
-
-      if (b == 2) {
-        status("Sent value to serial port");
-        Serial.println(numberBuffer);
-      }
-      // we dont really check that the text field makes sense
-      // just try to call
-      if (b == 0) {
-        status("Value cleared");
-        numberIndex = 0; // Reset index to 0
-        numberBuffer[numberIndex] = 0; // Place null in buffer
-      }
-
-      // Update the number display field
-      tft.setTextDatum(TL_DATUM);        // Use top left corner as text coord datum
-      tft.setFreeFont(&FreeSans18pt7b);  // Choose a nicefont that fits box
-      tft.setTextColor(DISP_TCOLOR);     // Set the font colour
-
-      // Draw the string, the value returned is the width in pixels
-      int xwidth = tft.drawString(numberBuffer, DISP_X + 4, DISP_Y + 12);
-
-      // Now cover up the rest of the line up by drawing a black rectangle.  No flicker this way
-      // but it will not work with italic or oblique fonts due to character overlap.
-      tft.fillRect(DISP_X + 4 + xwidth, DISP_Y + 1, DISP_W - xwidth - 5, DISP_H - 2, TFT_BLACK);
-
-      delay(10); // UI debouncing
     }
-  }
+    if (start = true){
+      for millis()
+    }
+    else{
+        tft.fillCircle(50,100,30,TFT_RED);
+    }
 }
